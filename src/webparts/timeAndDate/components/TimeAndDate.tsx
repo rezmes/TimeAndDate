5da8cb37f272c6556a9c8df73e4fe9cf97ef01ca
rezmes <rezmes@gmail.com>
2025-01-20 16:38:04 +0330

    works Jalali-code.

--- src/webparts/timeAndDate/components/TimeAndDate.tsx
+++ src/webparts/timeAndDate/components/TimeAndDate.tsx
@@ -1,42 +1,32 @@
 // src/webparts/TimeAndDate/components/TimeAndDate.tsx
 import * as React from "react";
-
+import * as moment from "moment-jalaali";
 export interface ITimeAndDateState {
   time: Date;
 }
-
 class TimeAndDate extends React.Component<{}, ITimeAndDateState> {
   private timerID: number;
-
   constructor(props: {}) {
     super(props);
-    this.state = {
-      time: new Date(),
-    };
+    this.state = { time: new Date() };
   }
-
   componentDidMount(): void {
     this.timerID = window.setInterval(() => this.tick(), 1000);
   }
-
   componentWillUnmount(): void {
     clearInterval(this.timerID);
   }
-
   tick(): void {
-    this.setState({
-      time: new Date(),
-    });
+    this.setState({ time: new Date() });
   }
-
   render() {
+    const jDate = moment(this.state.time).format("jYYYY/jM/jD");
     return (
       <div>
-        <h1>{this.state.time.toLocaleTimeString()}</h1>
-        <p>{this.state.time.toLocaleDateString()}</p>
+        {" "}
+        <h1>{this.state.time.toLocaleTimeString()}</h1> <p>{jDate}</p>{" "}
       </div>
     );
   }
 }
-
 export default TimeAndDate;
